d50bc30378cc7277aa8d9774b427d0afb6cfd944
Rob Ranf <rob@emiyaconsulting.com>
2023-07-17 17:38:06 -0700

    adding sqlite inmem db and statements

--- index-ws.js
+++ index-ws.js
@@ -2,6 +2,7 @@ const express = require("express");
 const server = require("http").createServer();
 const app = express();
 const WebSocketServer = require("ws").Server;
+const sqlite = require("sqlite3");
 
 app.get("/", (req, res) => {
 	res.sendFile("/index.html", { root: __dirname });
@@ -10,6 +11,15 @@ app.get("/", (req, res) => {
 server.on("request", app);
 server.listen(3000, () => {
 	console.log("Listening on 3000");
+});
+
+process.on("SIGINT", () => {
+	wss.clients.forEach(function each(client) {
+		client.close();
+	});
+	server.close(() => {
+		shutdownDB();
+	});
 });
 
 /** Websocket */
@@ -25,6 +35,10 @@ wss.on("connection", function connection(ws) {
 		ws.send("Welcome to my server");
 	}
 
+	db.run(`INSERT INTO visitors (count, time)
+				VALUES (${numClients}, datetime("now"))
+	`);
+
 	ws.on("close", function close() {
 		wss.broadcast(`Current visitors: ${numClients}`);
 		console.log("A client has disconnected");
@@ -36,3 +50,33 @@ wss.broadcast = function broadcast(data) {
 		client.send(data);
 	});
 };
+
+/** Database */
+const db = new sqlite.Database(":memory:");
+// const dbFile = new sqlite.Database("./fsfe.db") If wanted to use a db file vs in-mem
+
+// We're writing plain sql here as it doesn't matter for this exercise, but typically we'd use prepared statements
+db.serialize(() => {
+	db.run(`
+		CREATE TABLE visitors (
+			count INTEGER, 
+			time TEXT
+		)
+	`);
+});
+
+function getCounts() {
+	db.each(`SELECT * FROM visitors`, (err, row) => {
+		try {
+			console.log(row);
+		} catch (error) {
+			console.error(err);
+		}
+	});
+}
+
+function shutdownDB() {
+	getCounts();
+	console.log("Shutting down db");
+	db.close();
+}
